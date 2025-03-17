74737158670622db99321ec7bae29b3d31787986
pupajupa <garilarulit@gmail.com>
2025-03-17 19:29:48 +0300

    update output on compare_schema procedure

--- Database/LR3/DATA.sql
+++ Database/LR3/DATA.sql
@@ -35,6 +35,13 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE('Привет, мир!');
 END hello_world;
 
+ALTER SESSION SET CURRENT_SCHEMA = C##dev_schema;
+
+-- Индекс для таблицы DEV_TABLE1
+CREATE INDEX idx_dev_table1_name ON dev_table1(name);
+
+-- Уникальный индекс для таблицы DEPARTMENTS
+CREATE UNIQUE INDEX idx_departments_name ON departments(department_name);
 
 ALTER SESSION SET CURRENT_SCHEMA = C##prod_schema;
 
@@ -52,8 +59,15 @@ CREATE TABLE common_table (
     email VARCHAR2(100)
 );
 
+ALTER SESSION SET CURRENT_SCHEMA = C##prod_schema;
 
-ALTER SESSION SET CURRENT_SCHEMA = C#dev_schema;
+-- Индекс для таблицы PROD_TABLE1
+CREATE INDEX idx_prod_table1_name ON prod_table1(name);
+
+-- Составной индекс для таблицы COMMON_TABLE
+CREATE INDEX idx_common_table_name_email ON common_table(name, email);
+
+ALTER SESSION SET CURRENT_SCHEMA = C##dev_schema;
 
 CREATE TABLE departments (
     department_id NUMBER PRIMARY KEY,
@@ -92,9 +106,46 @@ ADD CONSTRAINT fk_team FOREIGN KEY (team_id) REFERENCES teams(team_id);
 ALTER TABLE teams
 ADD CONSTRAINT fk_lead FOREIGN KEY (lead_id) REFERENCES team_leads(lead_id);
 
+ALTER SESSION SET CURRENT_SCHEMA = C##dev_schema;
+
+CREATE OR REPLACE PACKAGE pkg_example IS
+    PROCEDURE calculate_bonus(emp_id NUMBER);
+    FUNCTION get_employee_count RETURN NUMBER;
+END pkg_example;
+
+CREATE OR REPLACE PACKAGE BODY pkg_example IS
+    PROCEDURE calculate_bonus(emp_id NUMBER) IS
+    BEGIN
+        DBMS_OUTPUT.PUT_LINE('Бонус для сотрудника ' || emp_id);
+    END;
+
+    FUNCTION get_employee_count RETURN NUMBER IS
+        v_count NUMBER;
+    BEGIN
+        SELECT COUNT(*) INTO v_count FROM employees;
+        RETURN v_count;
+    END;
+END pkg_example;
+
+
+ALTER SESSION SET CURRENT_SCHEMA = C##prod_schema;
+
+CREATE OR REPLACE PACKAGE pkg_example IS
+    PROCEDURE calculate_bonus(emp_id NUMBER);
+    -- В PROD-схеме функция get_employee_count отсутствует
+END pkg_example;
+
+CREATE OR REPLACE PACKAGE BODY pkg_example IS
+    PROCEDURE calculate_bonus(emp_id NUMBER) IS
+    BEGIN
+        DBMS_OUTPUT.PUT_LINE('Бонус для сотрудника ' || emp_id || ' (PROD)');
+    END;
+END pkg_example;
+
 SET SERVEROUTPUT ON SIZE UNLIMITED;
 
 ALTER SESSION SET CURRENT_SCHEMA = C##admin_schema;
 BEGIN
     compare_schemes('C##DEV_SCHEMA', 'C##PROD_SCHEMA');
 END;
+
